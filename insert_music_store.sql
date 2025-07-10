4b6e894d15b6eb902b235c6dd136d6199f1d6f72
Sophia <s89110227947@yandex.ru>
2025-07-11 00:37:37 +0300

    Add insert tables

--- insert_music_store.sql
+++ insert_music_store.sql
@@ -12,6 +12,16 @@ INSERT INTO Genre (genre_name) VALUES ('Поп');
 
 INSERT INTO Genre (genre_name) VALUES ('Электропоп');
 
+INSERT INTO SingerGenre (singer_id, genre_id) VALUES (1, 2);
+
+INSERT INTO SingerGenre (singer_id, genre_id) VALUES (2, 2);
+
+INSERT INTO SingerGenre (singer_id, genre_id) VALUES (3, 1);
+
+INSERT INTO SingerGenre (singer_id, genre_id) VALUES (4, 3);
+
+INSERT INTO SingerGenre (singer_id, genre_id) VALUES (4, 2);
+
 INSERT INTO
     Album (name, album_year)
 VALUES (
@@ -32,6 +42,14 @@ VALUES (
         2021
     );
 
+INSERT INTO SingerAlbum (singer_id, album_id) VALUES (1, 1);
+
+INSERT INTO SingerAlbum (singer_id, album_id) VALUES (2, 2);
+
+INSERT INTO SingerAlbum (singer_id, album_id) VALUES (3, 4);
+
+INSERT INTO SingerAlbum (singer_id, album_id) VALUES (4, 3);
+
 INSERT INTO
     Track (name, duration, album_id)
 VALUES ('Перезаряжай', 3.1, 4);
@@ -42,19 +60,27 @@ VALUES ('Вода', 3.13, 4);
 
 INSERT INTO
     Track (name, duration, album_id)
-VALUES ('Cool', 3.29, 3);
+VALUES ('Cool', 3.29, 1);
 
 INSERT INTO
     Track (name, duration, album_id)
-VALUES ('Material Gir', 4.04, 2);
+VALUES ('Material Girl', 4.04, 2);
 
 INSERT INTO
     Track (name, duration, album_id)
-VALUES ('Bad Guy', 3.14, 1);
+VALUES ('Bad Guy', 3.14, 3);
 
 INSERT INTO
     Track (name, duration, album_id)
 VALUES ('The Last Time', 3.41, 5);
+
+INSERT INTO
+    Track (name, duration, album_id)
+VALUES ('Моя оборона', 2.4, NULL);
+
+INSERT INTO
+    Track (name, duration, album_id)
+VALUES ('Навылет', 3.35, NULL);
 
 INSERT INTO
     Collection (title, collection_year)
@@ -69,3 +95,17 @@ VALUES ('Осеннее настроение', 2023);
 INSERT INTO
     Collection (title, collection_year)
 VALUES ('Русский рок', 2021);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (1, 4);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (2, 4);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (3, 2);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (4, 3);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (5, 2);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (9, 1);
+
+INSERT INTO TrackCollection (track_id, collection_id) VALUES (10, 4);
